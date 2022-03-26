37d5cd83ce1cc751dec1e73bdbee9504fa0fc68e
Habibullah800 <mdhabibullah800@gmail.com>
2022-03-26 11:49:48 +0600

    changed-cheak

--- src/App.js
+++ src/App.js
@@ -9,17 +9,10 @@ function App() {
         <p>
           Edit hello <code>src/App.js</code> and save to reload.
         </p>
-        <a
 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
-    </div>
+    </div >
   );
 }
 
